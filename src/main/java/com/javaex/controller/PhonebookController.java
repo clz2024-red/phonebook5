1dc70a50b5824eecf105ff1e9ad8c7ffdbd3ecf0
clz2024.red <clz2024.red@gmail.com>
2024-02-27 13:17:33 +0900

    2024-02-27

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.PhonebookDao;
+import com.javaex.service.PhonebookService;
 import com.javaex.vo.PersonVo;
 
 @Controller
@@ -22,15 +23,60 @@ public class PhonebookController {
 	//메소드gs
 	//메소드일반
 	
-	//등록폼
-	//localhost:8080/phonebook5/phone/writeform
-	@RequestMapping(value="/phone/writeform", method = {RequestMethod.GET, RequestMethod.POST})
-	public String writeForm() {
-		System.out.println("PhonebookController.writeForm()");
+
+	// 수정
+	@RequestMapping(value="/phone/modify", method = {RequestMethod.GET, RequestMethod.POST})
+	public String modify(@ModelAttribute PersonVo personVo) {
+		System.out.println("PhonebookController.modify()");
 		
-		return "/WEB-INF/views/writeForm.jsp";
+		PhonebookService phonebookService = new PhonebookService();
+		phonebookService.exeModify(personVo);
+		
+		return "redirect:/phone/list";
 	}
 	
+	
+	
+	// 수정폼
+	@RequestMapping(value="/phone/modifyform", method = {RequestMethod.GET, RequestMethod.POST})
+	public String modifyForm(@RequestParam(value="no") int no, Model model) {
+		System.out.println("PhonebookController.modifyForm()");
+		
+		
+		PhonebookService phonebookService = new PhonebookService();
+		PersonVo personVo = phonebookService.exeModifyForm(no);
+		
+		model.addAttribute("personVo", personVo);
+		
+		return "modifyForm";
+	}
+
+	//삭제
+	@RequestMapping(value="/phone/delete", method = {RequestMethod.GET, RequestMethod.POST})
+	public String delete(@RequestParam("no") int no) {
+		System.out.println("PhonebookController.delete()");
+		
+		PhonebookService phonebookService = new PhonebookService();
+		phonebookService.exeDelete(no);
+		
+		return "redirect:/phone/list";
+	}
+	
+	
+	// 리스트
+	@RequestMapping(value="/phone/list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("PhonebookController.list()");
+		
+		PhonebookService phonebookService = new PhonebookService();
+		List<PersonVo> personList =  phonebookService.exeList();
+		
+		
+		
+		model.addAttribute("pList", personList);
+		
+		return "list";
+	}
 	
 	//등록2
 	//localhost:8080/phonebook5/phone/write?name=황일영&hp=010&compay=02
@@ -40,20 +86,17 @@ public class PhonebookController {
 		
 		System.out.println(personVo.toString());
 		
-		
-		//dao를 메모리에 올린다
-		PhonebookDao phonebookDao = new PhonebookDao();
-		
-		//dao.personInsert(vo) 저장한다
-		phonebookDao.personInsert(personVo);
+		//서비스를 메모리에 올리고
+		//서비스의 메소드 사용
+		PhonebookService phonebookService = new PhonebookService();
+		phonebookService.exeWrite(personVo);
 		
 		//리스트로 리다이렉트
-		
 		return "redirect:/phone/list";
 	}
 	
 	
-	
+
 	//등록
 	//localhost:8080/phonebook5/phone/write?name=황일영&hp=010&compay=02
 	@RequestMapping(value="/phone/write", method = {RequestMethod.GET, RequestMethod.POST})
@@ -69,32 +112,28 @@ public class PhonebookController {
 		//vo 묶는다
 		PersonVo personVo = new PersonVo(name, hp, company);
 		
-		//dao를 메모리에 올린다
-		PhonebookDao phonebookDao = new PhonebookDao();
-		
-		//dao.personInsert(vo) 저장한다
-		phonebookDao.personInsert(personVo);
+		//서비스를 메모리에 올리고
+		//서비스의 메소드 사용
+		PhonebookService phonebookService = new PhonebookService();
+		phonebookService.exeWrite(personVo);
+				
 		
 		//리스트로 리다이렉트
 		return "redirect:/phone/list";
 	}
 	
-	@RequestMapping(value="/phone/list", method = {RequestMethod.GET, RequestMethod.POST})
-	public String list(Model model) {
-		System.out.println("PhonebookController.list()");
+	
+	//등록폼
+	//localhost:8080/phonebook5/phone/writeform
+	@RequestMapping(value="/phone/writeform", method = {RequestMethod.GET, RequestMethod.POST})
+	public String writeForm() {
+		System.out.println("PhonebookController.writeForm()");
 		
-		PhonebookDao phonebookDao = new PhonebookDao();
-		
-		List<PersonVo> personList = phonebookDao.personSelect();
-		//System.out.println(personList);
-		
-		model.addAttribute("pList", personList);
-		
-		return "/WEB-INF/views/list.jsp";
+		return "writeForm";
 	}
 	
 	
-	
+
 	
 	
 	
