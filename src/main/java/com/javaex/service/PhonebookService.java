f9c036e5ecff9aa367c04602ffeec15a9161a7a5
clz2024.red <clz2024.red@gmail.com>
2024-02-27 13:18:04 +0900

    2024-02-27

---
+++ src/main/java/com/javaex/service/PhonebookService.java
@@ -0,0 +1,72 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import com.javaex.dao.PhonebookDao;
+import com.javaex.vo.PersonVo;
+
+public class PhonebookService {
+
+	//필드
+	//생성자
+	//메소드 gs
+	//메소디 일반
+	
+	
+	//수정폼
+	public int exeModify(PersonVo personVo) {
+		System.out.println("PhonebookService.exeModify()");
+		
+		PhonebookDao phonebookDao = new PhonebookDao();
+		int count = phonebookDao.personUpdate(personVo);
+		
+		return count;
+		
+	}
+	
+	//수정폼
+	public PersonVo exeModifyForm(int no) {
+		System.out.println("PhonebookService.exeModifyForm()");
+		
+		PhonebookDao phonebookDao = new PhonebookDao();
+		PersonVo personVo = phonebookDao.personSelectOne(no);
+		
+		return personVo;
+		
+	}
+	
+	
+	//삭제 
+	public int exeDelete(int no) {
+		System.out.println("PhonebookService.exeDelete()");
+		
+		PhonebookDao phonebookDao = new PhonebookDao();
+		int count = phonebookDao.personDelete(no);
+		
+		return count;
+	}
+	
+	
+	//리스트
+	public List<PersonVo> exeList() {
+		System.out.println("PhonebookService.exeList()");
+		
+		PhonebookDao phonebookDao = new PhonebookDao();
+		List<PersonVo> personList = phonebookDao.personSelect();
+		
+		return personList;
+	}
+	
+	
+	//등록
+	public int exeWrite(PersonVo personVo) {
+		System.out.println("PhonebookService.exeWrite()");
+		
+		//비지니스로직X
+		PhonebookDao phonebookDao = new PhonebookDao();
+		int count = phonebookDao.personInsert(personVo);
+		
+		return count;
+	}
+	
+}
