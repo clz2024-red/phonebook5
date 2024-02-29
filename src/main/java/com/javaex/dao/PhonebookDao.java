58283aeefd8e9f51d09071905a8bc339dbd51052
clz2024.red <clz2024.red@gmail.com>
2024-02-29 12:04:17 +0900

    2024-02-29

--- src/main/java/com/javaex/dao/PhonebookDao.java
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -1,13 +1,7 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,219 +15,74 @@ public class PhonebookDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+
+	// 삭제
+	public int personDelete(int no) {
+		System.out.println("PhonebookDao.personDelete()");
+	
+		int count = sqlSession.delete("phonebook.delete", no);
+		return count;
+	}
+
+	// 수정
+	public int personUpdate(PersonVo personVo) {
+		System.out.println("PhonebookDao.personUpdate()");
+		
+		int count = sqlSession.update("phonebook.update", personVo);
+		return count;
+	}
+	
+	
+	
+	// 수정폼(1개 데이터 가져오기)
+	// 1개 가져오기
+	public PersonVo personSelectOne(int no) {
+		System.out.println("PhonebookDao.personSelectOne()");
+		
+		PersonVo personVo = sqlSession.selectOne("phonebook.selectOne", no);
+		System.out.println(personVo);
+		
+		return personVo;
+	}
+	
+	// 1개 가져오기2
+	public PersonVo personSelectOne2(int no) {
+		System.out.println("PhonebookDao.personSelectOne2()");
+	
+		System.out.println(no);
+		//PersonVo personVo = sqlSession.selectOne("phonebook.selectOne2", no);
+		return null;
+	}
+	
+	
+	// 등록
+	public int personInsert(PersonVo personVo) {
+		System.out.println("PhonebookDao.personInsert()");
+		
+		int count = sqlSession.insert("phonebook.insert", personVo);
+		return count;
+	}
+	
+	
+	// 등록2
+	public int personInsert2(Map<String, String> pMap ) {
+		System.out.println("PhonebookDao.personInsert()2");
+		System.out.println(pMap);
+		
+		int count = sqlSession.insert("phonebook.insert2", pMap);
+		
+		return count;
+	}
+	
+	
+	
 	// 전체가져오기
 	public List<PersonVo> personSelect() {
 		System.out.println("PhonebookDao.personSelect()");
 
 		List<PersonVo> personList = sqlSession.selectList("phonebook.select");
 		System.out.println(personList);
-		return null;
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	// 필드
-	private Connection conn = null;
-	private PreparedStatement pstmt = null;
-	private ResultSet rs = null;
-
-	@Autowired
-	private DataSource dataSource;
-	
-	// 생성자
-	// 메소드-gs
-	// 메소드-일반
-	// 연결
-	public void getConnection() {
-		try {
-
-			// 2. Connection 얻어오기
-			conn = dataSource.getConnection();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-
-		}
-	}
-
-	// 종료
-	public void close() {
-		// 5. 자원정리
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-	}
-
-
-	// 등록
-	public int personInsert(PersonVo personVo) {
-		int count = -1;
-
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " insert into person ";
-			query += " values(null, ?, ?, ?) ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, personVo.getName());
-			pstmt.setString(2, personVo.getHp());
-			pstmt.setString(3, personVo.getCompany());
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 등록되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
-
-		return count;
-	}
-
-	// 삭제
-	public int personDelete(int no) {
-		int count = -1;
-
-		this.getConnection();
-
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " delete from person ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, no);
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 삭제되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
-
-		return count;
-	}
-
-	// 1개 가져오기
-	public PersonVo personSelectOne(int no) {
-
-		this.getConnection();
-
-		PersonVo personVo = null;
-
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " select person_id, ";
-			query += "	      name, ";
-			query += "        hp, ";
-			query += "	      company ";
-			query += " from person ";
-			query += " where person_id=? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, no);
-
-			// 실행
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {// 반복
-				int personId = rs.getInt("person_id");
-				String name = rs.getString("name");
-				String hp = rs.getString("hp");
-				String company = rs.getString("company");
-
-				// db에서 가져온 데이터 vo로 묶기
-				personVo = new PersonVo(personId, name, hp, company);
-
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
-
-		return personVo;
+		return personList;
 	}
 	
-	// 수정
-	public int personUpdate(PersonVo personVo) {
-		int count = -1;
-
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " update person ";
-			query += " set name=?, ";
-			query += " 	   hp=?, ";
-			query += "     company=? ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, personVo.getName());
-			pstmt.setString(2, personVo.getHp());
-			pstmt.setString(3, personVo.getCompany());
-			pstmt.setInt(4, personVo.getPersonId());
-			
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 수정되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
-
-		return count;
-	}
 }
--- src/main/java/com/javaex/service/PhonebookService.java
+++ src/main/java/com/javaex/service/PhonebookService.java
@@ -1,6 +1,8 @@
 package com.javaex.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,74 +13,91 @@ import com.javaex.vo.PersonVo;
 @Service
 public class PhonebookService {
 
-	//필드
+	// 필드
 	@Autowired
 	private PhonebookDao phonebookDao;
-	//생성자
-	//메소드 gs
-	//메소디 일반
-	
-	
-	//리스트
-	public List<PersonVo> exeList() {
-		System.out.println("PhonebookService.exeList()");
-		
-		//PhonebookDao phonebookDao = new PhonebookDao();
-		List<PersonVo> personList = phonebookDao.personSelect();
-		
-		return personList;
-	}
-	
-	
-	
-	
-	
-	
-	
-	//수정폼
-	public int exeModify(PersonVo personVo) {
-		System.out.println("PhonebookService.exeModify()");
-		
-		//PhonebookDao phonebookDao = new PhonebookDao();
-		int count = phonebookDao.personUpdate(personVo);
-		
-		return count;
-		
-	}
-	
-	//수정폼
-	public PersonVo exeModifyForm(int no) {
-		System.out.println("PhonebookService.exeModifyForm()");
-		
-		//PhonebookDao phonebookDao = new PhonebookDao();
-		PersonVo personVo = phonebookDao.personSelectOne(no);
-		
-		return personVo;
-		
-	}
-	
-	
-	//삭제 
+	// 생성자
+	// 메소드 gs
+	// 메소디 일반
+
+	// 삭제
 	public int exeDelete(int no) {
 		System.out.println("PhonebookService.exeDelete()");
-		
-		//PhonebookDao phonebookDao = new PhonebookDao();
+
+		// PhonebookDao phonebookDao = new PhonebookDao();
 		int count = phonebookDao.personDelete(no);
-		
+
 		return count;
 	}
-	
 
+	// 수정
+	public int exeModify(PersonVo personVo) {
+		System.out.println("PhonebookService.exeModify()");
+
+		int count = phonebookDao.personUpdate(personVo);
+		return count;
+
+	}
+
+	// 수정폼
+	public PersonVo exeModifyForm(int no) {
+		System.out.println("PhonebookService.exeModifyForm()");
+
+		PersonVo personVo = phonebookDao.personSelectOne(no);
+
+		return personVo;
+
+	}
+
+	// 수정폼
+	public PersonVo exeModifyForm2(int no) {
+		System.out.println("PhonebookService.exeModifyForm2()");
+		
+		PersonVo personVo =phonebookDao.personSelectOne2(no);
+		return personVo;
+	}
 	
-	//등록
+	
+	
+	
+	// 등록
 	public int exeWrite(PersonVo personVo) {
 		System.out.println("PhonebookService.exeWrite()");
-		
-		//비지니스로직X
-		//PhonebookDao phonebookDao = new PhonebookDao();
+
+		// 비지니스로직X
+		// PhonebookDao phonebookDao = new PhonebookDao();
 		int count = phonebookDao.personInsert(personVo);
+
+		return count;
+	}
+
+	//등록2
+	public int exeWrite2(String name, String hp, String company) {
+		System.out.println("PhonebookService.exeWrite2()");
+		System.out.println(name);
+		System.out.println(hp);
+		System.out.println(company);
+		
+		//PersonVo를 제작해서 묶는다 --> 그런데 딱1번만쓸거 같다 --> map사용
+		Map<String, String> personMap = new HashMap<String, String>();
+		personMap.put("name", name);
+		personMap.put("hp", hp);
+		personMap.put("company", company);
+	
+		int count = phonebookDao.personInsert2(personMap);
 		
 		return count;
 	}
 	
+	
+	// 리스트
+	public List<PersonVo> exeList() {
+		System.out.println("PhonebookService.exeList()");
+
+		// PhonebookDao phonebookDao = new PhonebookDao();
+		List<PersonVo> personList = phonebookDao.personSelect();
+
+		return personList;
+	}
+
 }
